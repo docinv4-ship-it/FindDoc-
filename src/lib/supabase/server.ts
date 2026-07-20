c8b70b56ea2eb79d20a14fc910ebff2226b8546b
sheraz khan <docinv4@gmail.com>
2026-07-20 19:50:37 +0500

    Update server.ts

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -14,11 +14,11 @@ export async function createClient() {
         },
         setAll(cookiesToSet) {
           try {
-            cookiesToSet.forEach(({ name, value, options }) => {
-              cookieStore.set(name, value, options);
-            });
+            cookiesToSet.forEach(({ name, value, options }) =>
+              cookieStore.set(name, value, options)
+            );
           } catch {
-            // The `setAll` method was called from a Server Component.
+            // Ignored when called from Server Components directly
           }
         },
       },
